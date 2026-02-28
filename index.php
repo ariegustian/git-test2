d113618c20881959bb9306431f1c0286b7f799c0
ariegustian <ariegustian514@gmail.com>
2026-02-28 21:22:23 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem mollitia commodi accusantium explicabo quod sit ipsam earum delectus id quos vitae error cupiditate ratione atque ipsa, incidunt vel, architecto magni?</p>
 </body>
 </html>
